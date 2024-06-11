064593eb164cd93856534e36348b0abed8ef97da
leon_2192 <leon.strokes@gmail.com>
2024-06-11 13:44:39 -0300

    feature not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold text-center mb-4">404</h1>
+      <div className="mb-8">
+        <Image src="/images/404.png" alt="404" width={300} height={300} />
+      </div>
+      <h2 className="text-3xl font-semibold text-center mb-2">
+        Ups, parece que el enlace no se encontro!
+      </h2>
+      <p className="text-lg text-center mb-6">
+        Pero no te preocupes, puedes volver a intentar.
+      </p>
+      <Link href="/">
+        <button className="bg-gradient-to-r from-red-500 to-orange-500 hover:from-red-600 hover:to-orange-600 text-white py-3 px-6 rounded-lg text-lg">
+          Inicio
+        </button>
+      </Link>
+    </main>
+  );
+}
