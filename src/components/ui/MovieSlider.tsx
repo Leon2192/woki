a0bc448e70b43bbd97cb2e82c19720d63cc5105e
leon2192 <leon.strokes@gmail.com>
2024-06-11 00:39:01 -0300

    fix icon slider

--- src/components/ui/MovieSlider.tsx
+++ src/components/ui/MovieSlider.tsx
@@ -98,9 +98,9 @@ const MovieSlider = () => {
                     <Link href={`/movie/${movie.id}`} passHref>
                       <IconButton 
                         icon={
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
-                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                        </svg>                        
                         } 
                         text='More info' 
                       />
